86a671d04802ed766405be3f43236e01807387ff
Tim Barani <timb63701@gmail.com>
2023-11-06 19:25:55 -0600

    fixed animated gif freezing

--- content.js
+++ content.js
@@ -141,33 +141,93 @@ style.sheet.insertRule(
   0
 );
 
+let flashObserver = null;
+let freezeGifsOnScroll = null;
 function flashContent(enabled) {
-  if (enabled) {
-    // Stop CSS animations and transitions
-    const css = document.createElement("style");
-    css.innerHTML = `* { animation-play-state: paused !important; transition: none !important; }`;
-    document.head.appendChild(css);
+  const cssAnimationsStyleElement = document.createElement('style');
+  document.head.appendChild(cssAnimationsStyleElement);
 
-    // Stop GIFs by replacing them with a static image of the first frame
-    const gifs = document.querySelectorAll('img[src*=".gif"]');
-    gifs.forEach((gif) => {
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      const image = new Image();
-      image.src = gif.src;
-      image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
-        context.drawImage(image, 0, 0, image.width, image.height);
-        gif.src = canvas.toDataURL("image/png");
-      };
+  if (enabled) {
+    // Freeze CSS animations and transitions
+    cssAnimationsStyleElement.textContent = `* {
+      animation-play-state: paused !important;
+      transition: none !important;
+    }`;
+
+    // Freeze GIF images
+    [].slice.apply(document.images).filter(isGif).map(freezeGif);
+
+    // Pause autoplay videos
+    const videos = document.querySelectorAll('video[autoplay]');
+    videos.forEach(video => {
+      video.pause();
     });
 
-    // Pause HTML5 videos
-    const videos = document.querySelectorAll("video");
-    videos.forEach((video) => video.pause());
+    // Set up a MutationObserver to watch for new images being added to the DOM
+    flashObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          // Freeze new GIF images
+          if (node.tagName === 'IMG' && isGif(node)) {
+            freezeGif(node);
+          }
+          // If a new node with child images is added, check those images too
+          if (node.getElementsByTagName) {
+            [].slice.apply(node.getElementsByTagName('img')).filter(isGif).map(freezeGif);
+          }
+          // Pause new videos that are added to the DOM
+          if (node.tagName === 'VIDEO' && node.autoplay) {
+            node.pause();
+          }
+        });
+      });
+    });
+
+    flashObserver.observe(document.body, { childList: true, subtree: true });
+
+    // Add event listener for scroll events to handle any lazy-loaded GIFs
+    freezeGifsOnScroll = window.addEventListener('scroll', function() {
+      [].slice.apply(document.images).filter(isGif).map(freezeGif);
+    });
+
   } else {
-    document.reload();
+    // Resume CSS animations and transitions
+    cssAnimationsStyleElement.textContent = '';
+
+    // Resume autoplay videos
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+      if (video.paused) {
+        video.play();
+      }
+    });
+
+    // Disconnect the observer and remove the scroll event listener
+    flashObserver.disconnect();
+    window.removeEventListener('scroll', freezeGifsOnScroll);
+
+    // Reload the page to reset GIFs and videos
+    document.location.reload();
+  }
+}
+
+function isGif(i) {
+  return /^(?!data:).*\.gif/i.test(i.src);
+}
+
+function freezeGif(i) {
+  var c = document.createElement('canvas');
+  var w = c.width = i.width;
+  var h = c.height = i.height;
+  c.getContext('2d').drawImage(i, 0, 0, w, h);
+  try {
+    i.src = c.toDataURL("image/gif"); // Try to change the source of the image
+  } catch (e) {
+    // Cross-domain images will throw an error, we handle them here
+    for (var j = 0, a; a = i.attributes[j]; j++) {
+      c.setAttribute(a.name, a.value);
+    }
+    i.parentNode.replaceChild(c, i);
   }
 }
 
