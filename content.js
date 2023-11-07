212b1eee383e13450d000cc45e2d729e1a35355d
Tim Barani <timb63701@gmail.com>
2023-11-06 19:27:44 -0600

    formatted content file

--- content.js
+++ content.js
@@ -144,7 +144,7 @@ style.sheet.insertRule(
 let flashObserver = null;
 let freezeGifsOnScroll = null;
 function flashContent(enabled) {
-  const cssAnimationsStyleElement = document.createElement('style');
+  const cssAnimationsStyleElement = document.createElement("style");
   document.head.appendChild(cssAnimationsStyleElement);
 
   if (enabled) {
@@ -158,25 +158,28 @@ function flashContent(enabled) {
     [].slice.apply(document.images).filter(isGif).map(freezeGif);
 
     // Pause autoplay videos
-    const videos = document.querySelectorAll('video[autoplay]');
-    videos.forEach(video => {
+    const videos = document.querySelectorAll("video[autoplay]");
+    videos.forEach((video) => {
       video.pause();
     });
 
     // Set up a MutationObserver to watch for new images being added to the DOM
-    flashObserver = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        mutation.addedNodes.forEach(node => {
+    flashObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
           // Freeze new GIF images
-          if (node.tagName === 'IMG' && isGif(node)) {
+          if (node.tagName === "IMG" && isGif(node)) {
             freezeGif(node);
           }
           // If a new node with child images is added, check those images too
           if (node.getElementsByTagName) {
-            [].slice.apply(node.getElementsByTagName('img')).filter(isGif).map(freezeGif);
+            [].slice
+              .apply(node.getElementsByTagName("img"))
+              .filter(isGif)
+              .map(freezeGif);
           }
           // Pause new videos that are added to the DOM
-          if (node.tagName === 'VIDEO' && node.autoplay) {
+          if (node.tagName === "VIDEO" && node.autoplay) {
             node.pause();
           }
         });
@@ -186,17 +189,16 @@ function flashContent(enabled) {
     flashObserver.observe(document.body, { childList: true, subtree: true });
 
     // Add event listener for scroll events to handle any lazy-loaded GIFs
-    freezeGifsOnScroll = window.addEventListener('scroll', function() {
+    freezeGifsOnScroll = window.addEventListener("scroll", function () {
       [].slice.apply(document.images).filter(isGif).map(freezeGif);
     });
-
   } else {
     // Resume CSS animations and transitions
-    cssAnimationsStyleElement.textContent = '';
+    cssAnimationsStyleElement.textContent = "";
 
     // Resume autoplay videos
-    const videos = document.querySelectorAll('video');
-    videos.forEach(video => {
+    const videos = document.querySelectorAll("video");
+    videos.forEach((video) => {
       if (video.paused) {
         video.play();
       }
@@ -204,7 +206,7 @@ function flashContent(enabled) {
 
     // Disconnect the observer and remove the scroll event listener
     flashObserver.disconnect();
-    window.removeEventListener('scroll', freezeGifsOnScroll);
+    window.removeEventListener("scroll", freezeGifsOnScroll);
 
     // Reload the page to reset GIFs and videos
     document.location.reload();
@@ -216,15 +218,15 @@ function isGif(i) {
 }
 
 function freezeGif(i) {
-  var c = document.createElement('canvas');
-  var w = c.width = i.width;
-  var h = c.height = i.height;
-  c.getContext('2d').drawImage(i, 0, 0, w, h);
+  var c = document.createElement("canvas");
+  var w = (c.width = i.width);
+  var h = (c.height = i.height);
+  c.getContext("2d").drawImage(i, 0, 0, w, h);
   try {
     i.src = c.toDataURL("image/gif"); // Try to change the source of the image
   } catch (e) {
     // Cross-domain images will throw an error, we handle them here
-    for (var j = 0, a; a = i.attributes[j]; j++) {
+    for (var j = 0, a; (a = i.attributes[j]); j++) {
       c.setAttribute(a.name, a.value);
     }
     i.parentNode.replaceChild(c, i);
